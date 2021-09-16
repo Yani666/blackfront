7a1686099c61c01e1adecbd25f9e8d426f95ba12
Yani666 <yanis051989@gmail.com>
2021-09-16 10:01:42 -0500

    deploy

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const studio = process.env.NODE_ENV === 'studio';
+const studio = process.env.NODE_ENV === 'production';
 
-const baseURL = studio ? 'www.studio.com' : "http://localhost:3000/api"; 
+const baseURL = studio ? 'https://blackstudio.herokuapp.com/api' : "http://localhost:3000/api"; 
 //const baseURL = "https://blackstudio.herokuapp.com/api"
 export const _api = axios.create({
     baseURL,
