a8ada8561d08417e3a29dc01edfc1a4bdf6736a4
Yani666 <yanis051989@gmail.com>
2021-09-15 23:38:31 -0500

    ya?

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const studio = process.env.NODE_ENV === 'studio';
 
-/////const baseURL = studio ? 'www.studio.com' : "http://localhost:3000/api"; 
-const baseURL = "https://blackstudio.herokuapp.com/api"
+const baseURL = studio ? 'www.studio.com' : "http://localhost:3000/api"; 
+//const baseURL = "https://blackstudio.herokuapp.com/api"
 export const _api = axios.create({
     baseURL,
     withCredentials:true
